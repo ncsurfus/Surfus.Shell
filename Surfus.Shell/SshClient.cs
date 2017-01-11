400d7fd18a186ce3039fea4bb56ebff1b26d5aec
Surfus, Nathan <nathan.surfus@asurion.com>
2017-01-10 18:52:43 -0600

    Initial Commit

---
+++ Surfus.Shell/SshClient.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Surfus.Shell
+{
+    public class SshClient
+    {
+        public TaskCompletionSource<bool> ThreadTask = new TaskCompletionSource<bool>();
+
+        public Random _randomTest = new Random(DateTime.Today.Millisecond);
+        public string Hostname { get; }
+        public uint Port { get; }
+
+        public SshClient(string hostname) : this(hostname, 22)
+        {
+        }
+
+        public SshClient(string hostname, uint port)
+        {
+            Hostname = hostname;
+            Port = port;
+        }
+
+        public async Task StartRandomAsync()
+        {
+            await SshClientStaticThread.AddClientAsync(this);
+            await ThreadTask.Task;
+        }
+
+        internal async Task WaitRandomAsync()
+        {
+            var random = _randomTest.Next(1, 4);
+            Console.WriteLine($"{Hostname}: {random}");
+            if(random == 2)
+            {
+                throw new Exception("Crashed!");
+            }
+        }
+    }
+}
---
+++ Surfus.Shell/SshClientStaticThread.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Surfus.Shell
+{
+    public static class SshClientStaticThread
+    {
+        static SemaphoreSlim _internalSync = new SemaphoreSlim(1);
+        static List<ClientTask> _clients = new List<ClientTask>();
+        static TaskCompletionSource<bool> _updateThread = new TaskCompletionSource<bool>();
+        static Thread _internalThread;
+
+        public static async Task AddClientAsync(SshClient client)
+        {
+            await _internalSync.WaitAsync();
+            _clients.Add(new ClientTask { Client = client });
+
+            // Start Task thread if it is null or not alive
+            if (_internalThread?.IsAlive != true)
+            {
+                _internalThread = new Thread(() => { RunTasksAsync().Wait(); });
+                _internalThread.Start();
+            }
+
+            _updateThread.TrySetResult(true);
+            _internalSync.Release();
+        }
+
+        static async Task RunTasksAsync()
+        {
+            int taskCount;
+
+            // Get initial Count and initialize clients
+            await _internalSync.WaitAsync();
+            InitiailizeNewTasks();
+            _internalSync.Release();
+
+            while (taskCount > 0)
+            {
+                var completedTask = await Task.WhenAny(_clients.Select(x => x.Task).Union(new[] { _updateThread.Task }));
+
+                // Set new count and initialize clients
+                await _internalSync.WaitAsync();
+                if (completedTask == _updateThread.Task)
+                {
+                    InitiailizeNewTasks();
+                }
+                else if (completedTask.Exception != null)
+                {
+                    Console.WriteLine(completedTask.Exception.ToString());
+                    var client = _clients.First(x => x.Task == completedTask).Client;
+                    client.ThreadTask.TrySetException(completedTask.Exception);
+                    _clients.RemoveAll(x => x.Task == completedTask);
+                }
+                else
+                {
+                    var completedClient = _clients.First(x => x.Task == completedTask);
+                    completedClient.Task = completedClient.Client.WaitRandomAsync();
+                }
+                _internalSync.Release();
+            }
+
+            void InitiailizeNewTasks()
+            {
+                taskCount = _clients.Count;
+                foreach (var clientTask in _clients)
+                {
+                    if (clientTask.Task == null)
+                    {
+                        clientTask.Task = clientTask.Client.WaitRandomAsync();
+                    }
+                }
+            }
+        }
+
+        public class ClientTask
+        {
+            public SshClient Client { get; set; }
+            public Task Task { get; set; }
+        }
+    }
+}
