b634d18c1960f220e77180bb612fcc5f0ce4c048
Nathan Surfus <ncsurfus@gmail.com>
2017-01-10 21:39:13 -0600

    Import from Surfus.SecureShell

--- Surfus.Shell/KeyExchange/KeyExchangeAlgorithm.cs
+++ Surfus.Shell/KeyExchange/KeyExchangeAlgorithm.cs
@@ -22,8 +22,8 @@ using System.Text;
 using System.Threading.Tasks;
 using Surfus.Shell.Exceptions;
 using Surfus.Shell.Extensions;
-using Surfus.Shell.KeyExchange.DiffieHellman;
-using Surfus.Shell.KeyExchange.DiffieHellmanGroupExchange;
+//using Surfus.Shell.KeyExchange.DiffieHellman;
+//using Surfus.Shell.KeyExchange.DiffieHellmanGroupExchange;
 
 namespace Surfus.Shell.KeyExchange
 {
@@ -32,6 +32,8 @@ namespace Surfus.Shell.KeyExchange
     /// </summary>
     internal abstract class KeyExchangeAlgorithm
     {
+        private static readonly RandomNumberGenerator _randomGenerator = RandomNumberGenerator.Create();
+
         /// <summary>
         /// Supported key exchange algorithms.
         /// </summary>
@@ -65,14 +67,14 @@ namespace Surfus.Shell.KeyExchange
         {
             switch (exchangeResult.KeyExchangeAlgorithm)
             {
-                case "diffie-hellman-group-exchange-sha256":
+              /*  case "diffie-hellman-group-exchange-sha256":
                     return new DiffieHellmanGroupKeyExchange(client, exchangeResult, "SHA256");
                 case "diffie-hellman-group-exchange-sha1":
                     return new DiffieHellmanGroupKeyExchange(client, exchangeResult, "SHA1");
                 case "diffie-hellman-group14-sha1":
                     return new DiffieHellmanGroup14Sha1(client, exchangeResult);
                 case "diffie-hellman-group1-sha1":
-                    return new DiffieHellmanGroup1Sha1(client, exchangeResult);
+                    return new DiffieHellmanGroup1Sha1(client, exchangeResult);*/
                 default:
                     throw new SshException("Key Exchange Type Not Supported");
             }
@@ -134,7 +136,7 @@ namespace Surfus.Shell.KeyExchange
             var randomBytes = new byte[maxValue.ToByteArray().Length + 1];
             do
             {
-                SshClient.RandomGenerator.GetBytes(randomBytes);
+                _randomGenerator.GetBytes(randomBytes);
                 randomBytes[randomBytes.Length - 1] = 0;
                 randomValue = new BigInteger(randomBytes);
             }
