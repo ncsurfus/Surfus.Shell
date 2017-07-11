31bd9fd47db15dba002ef495bbfad1f3ea7b3fc2
Nathan Surfus <ncsurfus@gmail.com>
2017-07-11 18:53:24 -0500

    Revamped CiscoExtensions

--- Surfus.Shell.Cisco/CiscoExtensions.cs
+++ Surfus.Shell.Cisco/CiscoExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Surfus.Shell.Exceptions;
+using System;
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
@@ -7,73 +8,83 @@ namespace Surfus.Shell.Cisco
 {
     public static class CiscoExtensions
     {
-        /// <summary>
-        /// Attempts to match the prompt with the following [multiline] regex: ^(?<hostname>[^>\#\s]+)((?<privilegedPrompt>>)|(?<userPrompt>\#))\s*$
-        /// You can derive the hostname from the hostname group.
-        /// User/Privileged mode can be derived from the privilegedPrompt and userPrompt groups.
-        /// </summary>
-        /// <param name="terminal">The terminal this method extends.</param>
-        /// <param name="cancellationToken">A cancellation token to cancel the request.</param>
-        /// <returns></returns>
-        public static async Task<Match> GetFullPromptAsync(this SshTerminal terminal, CancellationToken cancellationToken)
+        public static async Task<CiscoTerminal> CiscoInitializeAndEnable(this SshTerminal terminal, string enablePassword, CancellationToken cancellationToken)
         {
-            var hostname = await terminal.ExpectRegexMatchAsync(@"^\s?(?<fullPrompt>(?<hostname>[^>\#\s]+)((?<privilegedPrompt>>)|(?<userPrompt>\#)))\s*$", RegexOptions.Multiline, cancellationToken).ConfigureAwait(false);
-            await terminal.WriteLineAsync("", cancellationToken).ConfigureAwait(false);
-            return hostname;
-        }
+            // Create a starting point.
+            await terminal.WriteLineAsync(cancellationToken);
 
-        public static async Task GetIosInitialDelimiter(this SshTerminal terminal, CancellationToken cancellationToken)
-        {
-            await terminal.WriteLineAsync(@"terminal length 0", cancellationToken).ConfigureAwait(false);
-            await terminal.ExpectAsync(@"terminal length 0", cancellationToken).ConfigureAwait(false);
-        }
+            // Get the prompt
+            var prompt = await terminal.GetPromptAsync(cancellationToken);
 
-        public static async Task GetAsaInitialDelimiter(this SshTerminal terminal, CancellationToken cancellationToken)
-        {
-            await terminal.WriteLineAsync(@"terminal pager 0", cancellationToken).ConfigureAwait(false);
-            await terminal.ExpectAsync(@"terminal pager 0", cancellationToken).ConfigureAwait(false);
-        }
-
-        public static async Task<TerminalMode> GetTerminalModeAsync(this SshTerminal terminal, CancellationToken cancellationToken)
-        {
-            var initialMode = await terminal.ExpectRegexMatchAsync(@"((?<privileged>\#)|(?<user>)>)\s*", cancellationToken).ConfigureAwait(false);
-
-            if (initialMode.Groups["privileged"].Success)
+            if (prompt.Groups["privileged"].Success)
             {
-                return TerminalMode.Privileged;
+                await terminal.FixTerminalLength(cancellationToken);
+                return new CiscoTerminal
+                {
+                    Prompt = prompt.Groups["fullPrompt"].Value,
+                    Name = prompt.Groups["hostname"].Value,
+                    Mode = TerminalMode.Privileged
+                };
             }
 
-            if (initialMode.Groups["user"].Success)
+            await terminal.GetEnableModeAsync(enablePassword, cancellationToken);
+            prompt = await terminal.GetPromptAsync(cancellationToken);
+            await terminal.FixTerminalLength(cancellationToken);
+            return new CiscoTerminal
             {
-                return TerminalMode.User;
-            }
-
-            throw new SshException("Unknown terminal mode");
+                Prompt = prompt.Groups["fullPrompt"].Value,
+                Name = prompt.Groups["hostname"].Value,
+                Mode = TerminalMode.Privileged
+            };
         }
 
-        public static async Task GetEnableModeAsync(this SshTerminal terminal, string enablePassword, CancellationToken cancellationToken)
+        private static async Task<Match> GetPromptAsync(this SshTerminal terminal, CancellationToken cancellationToken)
         {
-            // Get current state. Return if already at privileged.
-            var currentMode = await terminal.GetTerminalModeAsync(cancellationToken).ConfigureAwait(false);
-            if (currentMode == TerminalMode.Privileged)
+            Match prompt;
+            try
             {
-                return;
+                await terminal.WriteLineAsync(cancellationToken);
+                await terminal.WriteAsync("a bcdef", cancellationToken);
+                prompt = await terminal.ExpectRegexMatchAsync(@"^\s?(?<fullPrompt>(?<hostname>[^>\#\s]+)((?<user>>)|(?<privileged>\#)))\s*(?=a bcdef)", RegexOptions.Multiline, cancellationToken).ConfigureAwait(false);
+            }
+            catch (Exception ex)
+            {
+                throw new SshException("Failed to get prompt", ex);
             }
 
+            try
+            {
+                await terminal.WriteLineAsync(cancellationToken);
+                await terminal.ExpectAsync(prompt.Groups["fullPrompt"].Value, cancellationToken);
+            }
+            catch (Exception ex)
+            {
+                throw new SshException($"Failed to validate prompt: {prompt.Value}", ex);
+            }
+
+            return prompt;
+        }
+
+        private static async Task GetEnableModeAsync(this SshTerminal terminal, string enablePassword, CancellationToken cancellationToken)
+        {
             // Attempt to get enable prompt
             await terminal.WriteLineAsync("enable", cancellationToken).ConfigureAwait(false);
 
             var enablePrompt = await terminal.ExpectRegexMatchAsync(@"(?<passwordPrompt>(Password|password):?\s*)|((?<privileged>\#)|(?<user>)>)\s*", cancellationToken).ConfigureAwait(false);
+
+            // Server gave privileged prompt with no password.
             if (enablePrompt.Groups["privileged"].Success)
             {
                 return;
             }
 
+            // Server gave user prompt with no password.
             if (enablePrompt.Groups["user"].Success)
             {
                 throw new SshException("Server refused to provide enable password prompt");
             }
 
+            // Server gave us something else.
             if (!enablePrompt.Groups["passwordPrompt"].Success)
             {
                 throw new SshException("Unknown Password Prompt");
@@ -98,14 +109,27 @@ namespace Surfus.Shell.Cisco
             {
                 throw new SshException("Server rejected enable request and returned to password prompt");
             }
-
-            //Generate new prompt
         }
 
-        public enum TerminalMode
+        private static async Task FixTerminalLength(this SshTerminal terminal, CancellationToken cancellationToken)
         {
-            User,
-            Privileged
+            await terminal.WriteLineAsync("terminal length 0", cancellationToken);
+            await terminal.ExpectAsync("terminal length 0", cancellationToken);
+            await terminal.WriteLineAsync("terminal pager 0", cancellationToken);
+            await terminal.ExpectAsync("terminal pager 0", cancellationToken);
         }
+    }
+
+    public class CiscoTerminal
+    {
+        public string Prompt { get; set; }
+        public string Name { get; set; }
+        public TerminalMode Mode { get; set; }
+    }
+
+    public enum TerminalMode
+    {
+        User,
+        Privileged
     }
 }
