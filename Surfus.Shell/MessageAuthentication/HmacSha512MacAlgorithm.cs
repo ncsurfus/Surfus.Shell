7422ca669c258eaa30678c4ce9a8636900e23d6b
Nathan Surfus <ncsurfus@gmail.com>
2017-07-23 21:53:36 -0500

    More cleanup..

--- Surfus.Shell/MessageAuthentication/HmacSha512MacAlgorithm.cs
+++ Surfus.Shell/MessageAuthentication/HmacSha512MacAlgorithm.cs
@@ -36,13 +36,9 @@ namespace Surfus.Shell.MessageAuthentication
         public override bool VerifyMac(uint sequenceNumber, SshPacket sshPacket)
         {
             var computedMac = ComputeHash(sequenceNumber, sshPacket);
-            if (computedMac.Length < OutputSize)
-            {
-                return false;
-            }
             for (int i = 0; i != OutputSize; i++)
             {
-                if (sshPacket.Buffer[sshPacket.Buffer.Length - i] != computedMac[i])
+                if (sshPacket.Buffer[sshPacket.Buffer.Length - OutputSize + i] != computedMac[i])
                 {
                     return false;
                 }
